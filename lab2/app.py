7c8a0683e89231d94f782efa711b0a29167ce405
MoloZzz <mega_oleksii@ukr.net>
2024-05-16 13:27:42 +0300

    feat: errorHandler middleware feat: get Functions

--- lab2/app.py
+++ lab2/app.py
@@ -1,21 +1,37 @@
 from flask import jsonify, request, Flask
+from middleware.ErrorHandler import handle_exception
 from services.schedule_service import ScheduleService
 from dto.discipline import disciplineDTO
 
 
 app = Flask(__name__)
-service = ScheduleService()
+service = ScheduleService(app)
+handle_exception(app)
 
+@app.route('/discipline/<string:day>', methods=['GET'])
+def getOneDisciplineByDay(day):
+    return service.getManyDisciplineByDay(day)
 
-@app.route('/schedule/<string:day>', methods=['GET'])
-def get_course_controller(day):
-    return jsonify(service.get_schedule_day(day))
+@app.route('/disciplines', methods=['GET'])
+def getAllDisciplines():
+    return service.getManyDisciplines()
 
-@app.route('/add-course', methods=['POST'])
-def add_discipline_controller():
-    data = request.json
-    course_dto = disciplineDTO(data.get('name'), data.get('professor'), data.get('credits'))
-    return service.add_discipline(course_dto)
+@app.route('/discipline/<int:id>', methods=['GET'])
+def getOneDisciplines(id):
+    return service.getOneDisciplineById(id)
+
+@app.route('/discipline/add', methods=['POST'])
+def addDiscipline():
+    try:
+        data = request.json
+        name, professor, credits = data.get('name'), data.get('professor'), data.get('credits')
+        if name and professor and credits:
+            course_dto = disciplineDTO(name, professor, credits)
+            return service.addDiscipline(course_dto)
+        else:
+            return jsonify({"error": "Invalid input", "message": "Name, professor, and credits are required."}), 400
+    except ValueError as e:
+        return jsonify({"error": "Invalid input", "message": str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True, port=9001)
--- lab2/services/schedule_service.py
+++ lab2/services/schedule_service.py
@@ -1,35 +1,78 @@
 from flask import jsonify
+from sqlalchemy.exc import IntegrityError
 from db.schedule_db import schedule
-from db.models import Discipline
+from db.models import Discipline, Schedule
 from db.connection import connectDatabase
+from middleware.ErrorHandler import handle_exception
 
 class ScheduleService : 
-    def __init__(self):
+    def __init__(self,app):
         self.schedule = schedule
         self.session = connectDatabase()
+        self.app = app
+        handle_exception(self.app)
 
-    def get_schedule(self):
-        return schedule
+    def getOneDisciplineById(self,id):
+        try:
+            discipline = self.session.query(Discipline).filter_by(id=id).first()  
+            if discipline:  
+                return jsonify({
+                    "id": discipline.id,
+                    "name": discipline.name,
+                    "professor": discipline.professor,
+                    "credits": discipline.credits
+                }), 200  
+            else:
+                return jsonify({"error": "Discipline not found"}), 404 
+        except Exception as e:
+            return jsonify({"error": "Internal server error", "message": str(e)}), 500
 
-    def get_schedule_day(self, day):
-        if day in self.schedule:
-            return self.schedule[day]
-        else:
-            print("День", day, "не знайдено в розкладі.")
-            return None
+    def getManyDisciplineByDay(self, day):
+        try:
+            scheduleRecords = self.session.query(Schedule).filter_by(day_code=day).all()  
+            if scheduleRecords:
+                scheduleRecordsList = [{
+                    "id": scheduleRecord.id,
+                    "discipline_id": scheduleRecord.discipline_id,
+                    "day_code": scheduleRecord.day_code,
+                    "time": scheduleRecord.time
+                } for scheduleRecord in scheduleRecords]  
+                return scheduleRecordsList, 200 
+            else:
+                return jsonify({"error": "Discipline not found"}), 404 
+        except Exception as e:
+            return jsonify({"error": "Internal server error", "message": str(e)}), 500
+    
+    def getManyDisciplines(self):
+        try:
+            disciplines = self.session.query(Discipline).all()
+            disciplines_list = [{
+                "id": discipline.id,
+                "name": discipline.name,
+                "professor": discipline.professor,
+                "credits": discipline.credits
+            } for discipline in disciplines]
+            return jsonify(disciplines_list), 200
+        except Exception as e:
+            return jsonify({"error": "Internal server error", "message": str(e)}), 500
 
 
-    def add_discipline(self, discipline):
+    def addDiscipline(self, discipline):
+        try:
+            new_discipline = Discipline(name=discipline.name, professor=discipline.professor, credits=discipline.credits)
+            self.session.add(new_discipline)
+            self.session.commit()
+            self.session.refresh(new_discipline)  # можна прибрати, якщко потрібно оптимізувати
+            self.session.close()
 
-        new_discipline = Discipline(name=discipline.name, professor=discipline.professor, credits=discipline.credits)
-        self.session.add(new_discipline)
-        self.session.commit()
-        self.session.refresh(new_discipline) #можна прибрати, ящко потрібно оптимізувати
-        self.session.close()
-
-        return jsonify({
-            "id": new_discipline.id,
-            "name": new_discipline.name,
-            "professor": new_discipline.professor,
-            "credits": new_discipline.credits
-        }), 201
+            return jsonify({
+                "id": new_discipline.id,
+                "name": new_discipline.name,
+                "professor": new_discipline.professor,
+                "credits": new_discipline.credits
+            }), 201
+        except IntegrityError as e:
+            self.session.rollback() 
+            return jsonify({"error": "Database error", "message": str(e)}), 500
+        except Exception as e:
+            return jsonify({"error": "Internal server error", "message": str(e)}), 500
