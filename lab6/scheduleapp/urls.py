845e85f2f6cae29994f59a9eb5cfe4e29fa38aaf
MoloZzz <mega_oleksii@ukr.net>
2024-05-24 13:49:25 +0300

    feat: a lot( dont work correctly)

---
+++ lab6/scheduleapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from scheduleapp import views
+
+urlpatterns = [
+    path('schedule/', views.schedule_view, name='schedule'),
+    path('add_discipline/', views.add_discipline, name='add_discipline'),
+    path('add_schedule/<str:day>/', views.add_schedule, name='add_schedule'),
+]
